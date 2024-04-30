a827789ff8a2aa32c2b35525dfb6cdb51184f502
theMasterpc <mayanpotu500@gmail.com>
2024-04-30 16:54:05 +0300

    test

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/mayan/CLionProjects/mmn14
+# Install script for directory: C:/Users/mayan/Desktop/shared/mmn14_files
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/mayan/CLionProjects/mmn14/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/mayan/Desktop/shared/mmn14_files/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
