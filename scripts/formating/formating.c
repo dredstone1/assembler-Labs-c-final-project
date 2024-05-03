235c6650999519d877ebf56b01a9d0ebf5f093c3
theMasterpc <mayanpotu500@gmail.com>
2024-05-03 22:28:16 +0300

    file formating
    Signed-off-by: theMasterpc <mayanpotu500@gmail.com>

--- scripts/formating/formating.c
+++ scripts/formating/formating.c
@@ -1,2 +1,19 @@
 #include "formating.h"
+#include "../file.h"
+#include "../error.h"
 
+void format_line(file *file1, int line_number, error *error);
+
+void format_file(file *file1, error *error){
+    int i;
+
+    for (i = 0; i < file1->number_of_rows; i++){
+        format_line(file1, i, error);
+        if (error->error_type != NOTHING)
+            return;
+    }
+}
+
+void format_line(file *file1, int line_number, error *error){
+
+}
--- scripts/formating/formating.h
+++ scripts/formating/formating.h
@@ -1,4 +1,6 @@
 #ifndef MMN14_FORMATING_H
 #define MMN14_FORMATING_H
 
+void format_file(file *file1);
+
 #endif //MMN14_FORMATING_H
