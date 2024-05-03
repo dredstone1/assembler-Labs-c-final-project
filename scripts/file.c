2b16bdca7854ef6eb9d9a7cdb8af808b812fa2d7
theMasterpc <mayanpotu500@gmail.com>
2024-05-03 11:29:21 +0300

    binary translation functions
    Signed-off-by: theMasterpc <mayanpotu500@gmail.com>

--- scripts/file.c
+++ scripts/file.c
@@ -13,20 +13,22 @@ void read_file(file *file1){
     file1->line = malloc(lenmax);
 
     FILE *files = fopen(file1->filename, "r");
+
     if (files == NULL){
-        printf("Error: File not found\n");
+        perror("Error: File not found\n");
         exit(1);
     }
 
     while (bytesRead != 0){
         bytesRead = fread(&c, sizeof(char), 1, files);
-        if (c == '\n') {
+        if (c=='\n') {
             file1->line[file1->number_of_rows].content[i] = '\0';
-            lenmax += sizeof(line)*LINE_JUMPER_SIZE;
             file1->line = realloc(file1->line, lenmax);
             file1->number_of_rows++;
             i=0;
-        }else if( c=='\r')
+
+            lenmax += sizeof(line)*LINE_JUMPER_SIZE;
+        }else if(c=='\r')
             continue;
         else
             file1->line[file1->number_of_rows].content[i++] = c;
