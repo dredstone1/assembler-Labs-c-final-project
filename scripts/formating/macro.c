123cad1c356beaad85cdeb99a21344f4a8f65a7f
theMasterpc <mayanpotu500@gmail.com>
2024-05-08 17:46:50 +0300

    line formating system improvement
    Signed-off-by: theMasterpc <mayanpotu500@gmail.com>

--- scripts/formating/macro.c
+++ scripts/formating/macro.c
@@ -1,5 +1,31 @@
-//
-// Created by mayan on 08/05/2024.
-//
-
 #include "macro.h"
+
+bool is_line_macro(char line[LINE_SIZE]){
+    int i;
+    bool found_text = FALSE;
+
+    for (i = 0; i < MAX_TAG_SIZE; i++) {
+        if (line[i] == ' ' || line[i] == '\t') {
+            if (found_text)
+                return FALSE;
+        } else if (line[i] == '\n' || line[i] == '\0' || line[i] == '.')
+            return FALSE;
+        else if (line[i] == ':') {
+            if (found_text) {
+                return TRUE;
+            }
+
+            return FALSE;
+        } else {
+            if (is_legal_char_tag(line[i]) == FALSE)
+                return FALSE;
+
+            found_text = TRUE;
+            macros->macro_name.name[i] = line[i];
+        }
+    }
+    return FALSE;}
+
+void add_macro_to_line(line *line, macros macros, int line_number){
+
+}
