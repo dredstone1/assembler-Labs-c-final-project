c9157b02a328151823765fbbf6bdca0a95e38d02
theMasterpc <mayanpotu500@gmail.com>
2024-05-13 16:42:42 +0300

    line formating system improvement
    Signed-off-by: theMasterpc <mayanpotu500@gmail.com>

--- scripts/formating/directive_line_type.c
+++ scripts/formating/directive_line_type.c
@@ -1,8 +1,10 @@
 #include "directive_line_type.h"
+#include <stdio.h>
 
 bool is_directive_type_is(char line[], int *offset, char compare[]){
     int i = *offset;
 
+    printf("iii: %s ,,, %s\n", line, compare);
     for (; i < MAX_DIRECTIVE_SIZE; i++) {
         if(compare[i] == '\0'){
             *offset = i;
